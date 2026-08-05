1e80ef5678fc9c07b8f9fdd51a158c22df3b8f2d
LYK-CE <liyongkang_ce@163.com>
2026-08-05 13:15:03 +0800

    implement the text input node

---
+++ src/ui/text_input.gd
@@ -0,0 +1,16 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+# 按下发送按钮，然后将当前文本框里面的内容发送给LLM
+func _on_send_pressed() -> void:
+	pass # Replace with function body.
